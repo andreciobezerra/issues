fc4d4b9f8b8544cf069d0a2ee4a17dde4ab59027
Andrecio Bezerra <andreciobezerra@gmail.com>
2024-08-31 13:45:35 -0300

    decoding response and sorting

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -35,7 +35,22 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  defp process({user, project, _count}) do
+  defp process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+    |> sort_into_descending_order()
+    |> Enum.take(count)
+  end
+
+  defp decode_response({:ok, body}), do: body
+
+  defp decode_response({:error, error}) do
+    IO.puts("Error fetching from Github: #{error["message"]}")
+    System.halt(2)
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 -> i1["created_at"] >= i2["created_at"] end)
   end
 end
